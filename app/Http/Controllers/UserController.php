74b33414fd4b42f16568010a358c7766306ae7ff
Chandan kumar thakur <tchandankumar2004@gmail.com>
2026-04-02 14:24:07 +0530

    Insert,Updated,Delete operationn perfome with query_laravel some method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-   public function showUsers(){
-    $user =DB::table('students')->get();
+  ///All Users
+  public function showUsers()
+  {
+    $user = DB::table('students')->get();
 
-    return view("allusers",['data'=> $user]);
+    return view("allusers", ['data' => $user]);
 
     //Method4
     // foreach($user as $users){
@@ -19,11 +21,87 @@ class UserController extends Controller
     //Method3//dump($user);
     //Method2//dd($user);
     //Method1// return $user;
-   }
+  }
 
-   public function singleUser(string $id){
-     $user =DB::table('students')->where('id',$id)->get();
-     return view('user',['data' => $user ]);
+  //Signal User
+  public function singleUser(string $id)
+  {
+    $user = DB::table('students')->where('id', $id)->get();
+    return view('user', ['data' => $user]);
 
+  }
+  //Add user
+  public function addUser()
+  {
+    $user = DB::table('students')
+      ->insert([
+        [
+          'name' => 'Mohan Kumar',
+          'email' => 'Mohan@gmail.com',
+          'age' => 29,
+          'city' => 'Patna',
+          'created_at' => now(),
+          'updated_at' => now()
+
+        ],
+        [
+          'name' => 'Binayak Kumar',
+          'email' => 'Binayak@gmail.com',
+          'age' => 19,
+          'city' => 'Patna',
+          'created_at' => now(),
+          'updated_at' => now()
+
+        ],
+        [
+          'name' => 'Ram Kumar',
+          'email' => 'Ram@gmail.com',
+          'age' => 19,
+          'city' => 'Patna',
+          'created_at' => now(),
+          'updated_at' => now()
+
+        ]
+      ]);
+    if ($user) {
+      echo "<h1>Data Insert is Successful</h1>";
+    }
+  }
+
+  ///Updated User
+  public function updatedUser(){
+    $user = DB::table('students')
+              ->where('id',3)
+              ->update([
+                'city' => "Darbhanga",
+                'age' =>17
+              ]);
+
+
+              // ->where('id',2)
+              // ->update([
+              //   'city' => "Mumbai"
+              // ]);
+   if($user){
+     echo "<h1>Updated data</h1>";
    }
+  }
+
+
+  ///Delete User
+  public function deleteUser(string $id){
+    $user = DB::table('students')
+          ->where('id',$id)
+          ->delete();
+
+    if($user){
+      return redirect()->route('home');
+    }
+
+    // if($user){
+    //     echo "<h1>Delete Exist data</h1>";
+    // }
+  }
+  
+ 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-Route::get('/',[UserController::class,'showUsers']);
+Route::get('/',[UserController::class,'showUsers'])->name('home');
 Route::get('/user/{id}',[UserController::class,'singleUser'])->name('view.user');
-
-
+Route::get('/add',[UserController::class,'addUser']);
+Route::get('/update',[UserController::class,'updatedUser']);
+Route::get('/delete/{id}',[UserController::class,'deleteUser'])->name('delete.user');
 
 // Route::get('/', function () {
 //     return view('welcome');
